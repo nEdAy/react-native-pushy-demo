6b0c5bd4ba69c5d4e88a445b6ebce363a739b27f
Kevin SuTJ <shengsu15@gmail.com>
2022-12-21 17:35:54 +0800

    pushy PushyDemo 0.0.2

--- App.js
+++ App.js
@@ -82,7 +82,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="Step One[Has been hot updated]">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
